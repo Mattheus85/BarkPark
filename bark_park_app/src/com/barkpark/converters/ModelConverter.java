9f486fb7dc39d7833f59051811e2714d74708789
Thom Hammond <thomas_hammond@berkeley.edu>
2022-02-14 21:25:31 -0800

    Completed rough draft of ModelConverter with toParkModel method

--- bark_park_app/src/com/barkpark/converters/ModelConverter.java
+++ bark_park_app/src/com/barkpark/converters/ModelConverter.java
@@ -1,4 +1,26 @@
 package com.barkpark.converters;
 
+import com.barkpark.dynamodb.models.Park;
+import com.barkpark.models.ParkModel;
+
 public class ModelConverter {
+    /**
+     * Converts a provided {@link Park} into a {@link ParkModel} representation.
+     * @param park the park to convert
+     * @return the converted parkModel
+     */
+    // I'm implementing this method as static since I see no reason why we would need an instance of ModelConverter
+    // We can change to the Playlist approach if desired or discuss this further
+    public static ParkModel toParkModel(Park park) {
+        // Playlist checked if tags was empty and set the model to null if so...
+        // Need to review WHY this was being done and if we should do something similar
+
+        return ParkModel.builder()
+                .withId(park.getId())
+                .withName(park.getName())
+                .withLocation(park.getLocation())
+                .withAvgRating(park.getAvgRating())
+                .withTags(park.getTags())
+                .build();
+    }
 }
