afeb994841150297c57dedd50bacea7559353741
Thom Hammond <thomas_hammond@berkeley.edu>
2022-02-14 18:11:33 -0800

    Completed rough draft of ParkDao required for GetParkActivity and ParkNotFoundException as required by ParkDao

--- bark_park_app/src/com/barkpark/dynamodb/ParkDao.java
+++ bark_park_app/src/com/barkpark/dynamodb/ParkDao.java
@@ -1,4 +1,38 @@
 package com.barkpark.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+import com.barkpark.dynamodb.models.Park;
+import com.barkpark.exceptions.ParkNotFoundException;
+
+/**
+ * Accesses data for a park using {@link Park} to represent the model in DynamoDB.
+ */
 public class ParkDao {
+    private final DynamoDBMapper dynamoDbMapper;
+
+    /**
+     * Instantiates a ParkDao object.
+     *
+     * @param dynamoDbMapper the {@link DynamoDBMapper} used to interact with the parks table
+     */
+    public ParkDao(DynamoDBMapper dynamoDbMapper) {
+        this.dynamoDbMapper = dynamoDbMapper;
+    }
+
+    /**
+     * Returns the {@link Park} corresponding to the specified id.
+     *
+     * @param id the Park ID
+     * @return the stored Park, or throw {@link ParkNotFoundException} if none was found.
+     */
+    public Park getPark(String id) throws ParkNotFoundException {
+        Park park = this.dynamoDbMapper.load(Park.class, id);
+
+        if (park == null) {
+            throw new ParkNotFoundException("Could not find park with id " + id);
+        }
+
+        return park;
+    }
 }
---
+++ bark_park_app/src/com/barkpark/exceptions/ParkNotFoundException.java
@@ -0,0 +1,41 @@
+package com.barkpark.exceptions;
+
+/**
+ * Exception to throw when a given park ID is not found in the database.
+ */
+public class ParkNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 4619446731689544096L;
+
+    /**
+     * Exception with no message or cause.
+     */
+    public ParkNotFoundException() {
+        super();
+    }
+
+    /**
+     * Exception with a message, but no cause.
+     * @param message A descriptive message for this exception.
+     */
+    public ParkNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Exception with no message, but with a cause.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public ParkNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Exception with message and cause.
+     * @param message A descriptive message for this exception.
+     * @param cause The original throwable resulting in this exception.
+     */
+    public ParkNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
