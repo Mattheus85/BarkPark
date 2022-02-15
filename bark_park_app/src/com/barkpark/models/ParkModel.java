a4c78e0294bcbaef39f94588056e343f14d2c9dc
Matthew Mattheus <19mattheus85@gmail.com>
2022-02-15 15:33:02 -0800

    ParkModel & GetParkRequest

--- bark_park_app/src/com/barkpark/models/ParkModel.java
+++ bark_park_app/src/com/barkpark/models/ParkModel.java
@@ -21,6 +21,46 @@ public class ParkModel {
         this.tags = builder.tags;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(Integer avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- bark_park_app/src/com/barkpark/models/requests/GetParkRequest.java
+++ bark_park_app/src/com/barkpark/models/requests/GetParkRequest.java
@@ -15,6 +15,10 @@ public class GetParkRequest {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
