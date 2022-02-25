9903b81e1fdbe7b7966ec5a1b187d0e3878b47f8
Thom Hammond <thomas_hammond@berkeley.edu>
2022-02-25 14:52:43 -0800

    All the stuff

--- frontend/user.js
+++ frontend/user.js
@@ -66,3 +66,52 @@ function createTitleAndHeader() {
   let ti = document.createTextNode(user);
   title.appendChild(ti);
 }
+
+var BarkPark = window.BarkPark || {};
+BarkPark.map = BarkPark.map || {};
+
+(function rideScopeWrapper($) {
+  var authToken;
+    BarkPark.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
+
+    // Register click handler for #request button
+    $(function onDocReady() {
+      $('#request').click(handleRequestClick);
+      $(BarkPark.map).on('pickupChange', handlePickupChanged);
+
+      BarkPark.authToken.then(function updateAuthMessage(token) {
+          if (token) {
+              displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
+              $('.authToken').text(token);
+          }
+      });
+
+      if (!_config.api.invokeUrl) {
+          $('#noApiMessage').show();
+      }
+  });
+
+function handlePickupChanged() {
+  var requestButton = $('#request');
+  requestButton.text('Request Unicorn');
+  requestButton.prop('disabled', false);
+}
+function handleRequestClick(event) {
+  var pickupLocation = WildRydes.map.selectedPoint;
+  event.preventDefault();
+  requestUnicorn(pickupLocation);
+}
+
+function displayUpdate(text) {
+  $('#updates').append($('<li>' + text + '</li>'));
+}
+}(jQuery));
